828a9d4f719ddeb4ad9010fe610e202da9013048
lkoblue <gangwon20@gmail.com>
2024-08-03 15:59:37 +0900

    스프링 mvc 기본 기능 1

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,30 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Slf4j
+@RestController
+public class LogTestController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        // 아래와 같이 작성할 시 실제로 출력하지 않더라도 연산은 작동하므로
+        // 해당 방식보다 ={} 와 같이 작성하는 것이 좋다
+        // log.trace("trace log=" + name);
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+}
